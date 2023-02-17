7fa4dddd366ec622abb3de8c46f10326bd4a4d6d
BATUHAN ŞENOĞLU <batuhansenogluis@gmail.com>
2023-02-17 17:56:53 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+# define input text
+text = "This is a simple example sentence for NLP code."
+
+# tokenize the text
+tokens = word_tokenize(text)
+
+# print the tokens
+print(tokens)
